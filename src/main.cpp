e868c7cf31de4f48cd9f896fffb6ae1c6e64da2e
Magí Romanyà <magiromanya@gmail.com>
2023-02-16 15:07:55 +0100

    "Now the shader path is handled by cmake"

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,80 @@
 #include <iostream>
 #include <stdlib.h>
 
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+#include "renderer.h"
+#include "shader_path.h"
+#include "intersection.h"
 
-#include "glad/glad.h"
-#include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
 
-#include "mesh.h"
-#include "shader.h"
-#include "camera.h"
-#include "object.h"
-
-#define HEIGHT 800
-#define WIDTH 600
-
-GLFWwindow* CreateWindow();
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-void processInput(GLFWwindow* window);
-void render();
-void render_gui();
-
-Object obj;
-Camera camara;
 
 int main(int argc, char *argv[]) {
-
     std::cout << "The renderer started" << std::endl;
 
-    GLFWwindow* window = CreateWindow();
+    Renderer renderer = Renderer();
+    GLFWwindow* window = renderer.window;
 
     ///////////////////////////////
     SimpleMesh mesh = SimpleMesh();
 
-    CreateGrid(mesh, 5, 5, 1.0f);
+    CreateGrid(mesh, 5, 5, 10.0f);
 
-    Shader shader_test = Shader("../shaders/test.v0.vert", "../shaders/test.v0.frag");
+    Shader shader_test = Shader(SHADER_PATH"/test.v0.vert", SHADER_PATH"/test.v0.frag");
 
-    obj = Object(&mesh, shader_test);
-    obj.model = glm::translate(obj.model, glm::vec3(0.0f , 0.0f, -4.0f));
-    // obj.model = glm::rotate(obj.model, 3.14159f / 4.0f, glm::vec3(0, 1, 0));
-    obj.view = glm::lookAt(glm::vec3(0.0f,0.0f,0.0f), glm::vec3(0.0f,0.0f,-1.0f), glm::vec3(0.0f,1.0f,0.0f));
-    obj.proj = glm::perspective(30.0f, 1.0f, 0.1f, 10.f);
-    obj.loadTexture("gandalf", "../img/gandalf.png");
+    Object obj = Object(&mesh, shader_test);
+    obj.translation = glm::vec3(0 ,0, -100);
+    obj.rotation = glm::vec3(90,0,0);
+    obj.updateModelMatrix();
+    obj.loadTexture("gandalf", TEXTURE_PATH"/gandalf.png");
+    renderer.addObject(&obj);
+
+    SimpleMesh mesh2;
+    Shader shader_normal = Shader(SHADER_PATH"/test.v0.vert", SHADER_PATH"/normals.frag");
+    // mesh2.loadFromFile("../img/bunny.obj");
+    CreateBox(mesh2, 1, 1, 1);
+    Object obj2 = Object(&mesh2, shader_normal);
+    obj2.translation = glm::vec3(0,0,-10);
+    obj2.scaling = glm::vec3(1);
+    obj2.updateModelMatrix();
+    obj2.loadTexture("gandalf", TEXTURE_PATH"/gandalf.png");
+    renderer.addObject(&obj2);
+
+    SimpleMesh mesh3;
+    CreateBox(mesh3, 1, 1, 1);
+    Object obj3 = Object(&mesh3, shader_normal);
+    obj3.translation = glm::vec3(0,-5,-10);
+    obj3.scaling = glm::vec3(0.1f);
+    obj3.updateModelMatrix();
+    obj3.loadTexture("gandalf", TEXTURE_PATH"/gandalf.png");
+    renderer.addObject(&obj3);
+
+    // Axis rendering
+    Object xaxis = Object(&mesh3, shader_normal);
+    xaxis.translation = obj3.translation;
+    xaxis.scaling = glm::vec3(100.0f, 0.01f, 0.01f);
+    xaxis.updateModelMatrix();
+    renderer.addObject(&xaxis);
+    Object yaxis = Object(&mesh3, shader_normal);
+    yaxis.translation = obj3.translation;
+    yaxis.scaling = glm::vec3(0.01f, 100.0f, 0.01f);
+    yaxis.updateModelMatrix();
+    renderer.addObject(&yaxis);
+    Object zaxis = Object(&mesh3, shader_normal);
+    zaxis.translation = obj3.translation;
+    zaxis.scaling = glm::vec3(0.001f, 0.01f, 100.0f);
+    zaxis.updateModelMatrix();
+    renderer.addObject(&zaxis);
 
     // Render loop
     while (!glfwWindowShouldClose(window)){
         // Input
-        processInput(window);
+        renderer.cameraInput();
 
         // Render
-        render();
+        renderer.render();
+
+        if (is_inside(obj2,
+                      glm::vec3(obj3.model * glm::vec4(obj3.mesh->vertices[0].Position, 1.0f))))
+            std::cout << "The object is inside" << std::endl;
 
         // Swap buffers + check events
         glfwSwapBuffers(window);
@@ -63,144 +83,4 @@ int main(int argc, char *argv[]) {
 
     glfwTerminate();
     return 0;
-}
-
-GLFWwindow* CreateWindow(){
-    /* Initializes openGL context and creates a resizable window with a callback */
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Simulation", NULL, NULL);
-
-    if (window == NULL){
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        exit(-1);
-    }
-    glfwMakeContextCurrent(window);
-
-    // Window resize callback
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    // Mouse
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-    glfwSetCursorPosCallback(window, mouse_callback);
-
-    // Imgui context
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO& io = ImGui::GetIO(); (void) io;
-    ImGui::StyleColorsDark();
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init();
-
-    if (!gladLoadGLLoader( (GLADloadproc) glfwGetProcAddress )){
-        std::cout << "Failed to intitialize GLAD" << std::endl;
-        exit(-1);
-    }
-
-    return window;
-}
-
-void framebuffer_size_callback(GLFWwindow* window, int width, int height){
-    /* Gets called every time we resize the window */
-    glViewport(0, 0, width, height);
-    obj.proj = glm::perspective(30.0f, ( (float) width) / height, 0.1f, 10.f);
-}
-
-
-void processInput(GLFWwindow* window){
-    /* Controlls user input through the window */
-    if(glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS){
-        glfwSetWindowShouldClose(window, true);
-    }
-
-
-    float deltaTime = 0.0f;
-    static float lastFrame = 0.0f;
-    float currentFrame = glfwGetTime();
-    deltaTime = currentFrame - lastFrame;
-    lastFrame = currentFrame;
-
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS){
-        camara.ProcessKeyboard(FORWARD, deltaTime);
-    }
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS){
-        camara.ProcessKeyboard(BACKWARD, deltaTime);
-    }
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS){
-        camara.ProcessKeyboard(LEFT, deltaTime);
-    }
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS){
-        camara.ProcessKeyboard(RIGHT, deltaTime);
-    }
-
-    if (glfwGetKey(window, GLFW_KEY_G) == GLFW_PRESS){
-        obj.mesh->updateVAO();
-    }
-
-    // Enable / disable orbital cam
-    static bool escape_last_frame = false;
-    bool escape_this_frame = glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS;
-    if (escape_this_frame and (escape_this_frame != escape_last_frame)){
-        camara.is_orbital = !camara.is_orbital;
-        if (camara.is_orbital)
-            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-        else
-            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-    }
-    escape_last_frame = escape_this_frame;
-    obj.view = camara.GetViewMatrix();
-}
-
-void mouse_callback(GLFWwindow* window, double xpos, double ypos){
-    static float lastX = WIDTH/2.0f;
-    static float lastY = HEIGHT/2.0f;
-    float deltaX = xpos - lastX;
-    float deltaY = ypos - lastY;
-    lastX = xpos;
-    lastY = ypos;
-    camara.ProcessMouseMovement(deltaX, deltaY);
-}
-
-void render(){
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    obj.render();
-    render_gui();
-}
-
-void render_gui(){
-    // Start the frame
-    ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplGlfw_NewFrame();
-    ImGui::NewFrame();
-    {
-        static float f = 0.0f;
-        static int counter = 0;
-
-        ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!"
-                                       // and append into it.
-
-        ImGui::Text("This is some useful text."); // Display some text (you can
-        ImGui::SliderFloat(
-            "float", &f, 0.0f,
-            1.0f); // Edit 1 float using a slider from 0.0f to 1.0f
-        if (ImGui::Button(
-                "Button")) // Buttons return true when clicked (most widgets
-                           // return true when edited/activated)
-            counter++;
-        ImGui::SameLine();
-        ImGui::Text("counter = %d", counter);
-
-        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
-                    1000.0f / ImGui::GetIO().Framerate,
-                    ImGui::GetIO().Framerate);
-        ImGui::End();
-    }
-
-    ImGui::Render();
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
